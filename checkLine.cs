0b6288e96cb299042ff3d3d5666ca5b7df406483
Ashwini <salunkheashwini1998@gmail.com>
2022-12-29 15:19:26 +0530

    Checking Line

--- checkLine.cs
+++ checkLine.cs
@@ -19,10 +19,24 @@ namespace LineComparison
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter y2 :- ");
             int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a1 :- ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a2 :- ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b1 :- ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b2 :- ");
+            int b2 = Convert.ToInt32(Console.ReadLine());
 
-            double Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double Length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of First line is  :- " + Length1);
 
-            Console.WriteLine("Length of line is  :- " + Length);
+            double Length2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            Console.WriteLine("Length of Second line is  :- " + Length2);
+
+            // Console.WriteLine(System.Object.ReferenceEquals(Length1, Length2));
+            //Console.WriteLine("Equals = "+ (Length1==Length2));
+            Console.WriteLine("Equals = " + Length1.Equals(Length2));
         }
     }
 }
